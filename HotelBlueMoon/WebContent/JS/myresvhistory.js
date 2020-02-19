e79caecd5317f27a6f0904cc1a8ade2d0db06401
LeeSangYoeb <xyrho.v@gmail.com>
2020-02-19 19:51:53 +0900

    mypage resvhistory search debug success

--- HotelBlueMoon/WebContent/JS/myresvhistory.js
+++ HotelBlueMoon/WebContent/JS/myresvhistory.js
@@ -1,15 +1,25 @@
 const select = document.querySelector(".js-search-select");
 const searchBtn = document.querySelector(".js-searchBtn");
 const searchText = document.querySelector(".js-searchText");
+const loginId = document.querySelector(".js-session");
 
 const allListBtn = document.querySelector(".js-allListBtn");
+
+var nowTime = "";
+
+function currDate() {
+	const currDate = new Date();
+	const yyyy = currDate.getFullYear();
+	const MM = currDate.getMonth() + 1;
+	const dd = currDate.getDate();
+	nowTime = yyyy + "-" + MM + "-" + dd;
+};
 
 function getContextPath() {
 	var hostIndex = location.href.indexOf(location.host) + location.host.length;
 	return location.href.substring(hostIndex, location.href.indexOf('/',
 			hostIndex + 1));
 };
-
 
 function searchCheck() {
 	if (select.value === "검색") {
@@ -24,15 +34,18 @@ function searchCheck() {
 function searchEvent() {
 	$.ajax({
 		method : "get",
-		url : `${getContextPath()}` + "/fowardmyresvhistory",
+		url : `${getContextPath()}` + "/fowardmyresvhistory?loginId="
+				+ loginId.value + "&nowTime=" + nowTime,
 		data : {
 			"selectIndex" : `${select.value}`,
 			"searchText" : `${searchText.value}`
 		},
 		success : function(data) {
 			location.href = `${getContextPath()}`
-					+ "/fowardmyresvhistory?selectIndex=" + `${select.value}`
-					+ "&searchText=" + `${searchText.value}`
+					+ "/fowardmyresvhistory?loginId=" + loginId.value
+					+ "&nowTime=" + nowTime + "&selectIndex="
+					+ `${select.value}` + "&searchText="
+					+ `${searchText.value}`
 		},
 		error : function() {
 			alert("Ajax 실패")
@@ -41,16 +54,14 @@ function searchEvent() {
 }
 
 function allSearch() {
-	location.href = `${getContextPath()}` + "/fowardmyresvhistory";
+	location.href = `${getContextPath()}` + "/fowardmyresvhistory?loginId="
+			+ loginId.value + "&nowTime=" + nowTime;
 }
-
-
-
 
 function init() {
 	searchBtn.addEventListener("click", searchCheck);
 	allListBtn.addEventListener("click", allSearch);
-
+	currDate();
 }
 
 init();
