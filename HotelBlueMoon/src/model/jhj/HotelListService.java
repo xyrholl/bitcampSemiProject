65f98efa6f915a25bb90d69813f3c7665d9ee312
LeeSangYoeb <xyrho.v@gmail.com>
2020-02-19 16:02:54 +0900

    cal update

--- HotelBlueMoon/src/model/jhj/HotelListService.java
+++ HotelBlueMoon/src/model/jhj/HotelListService.java
@@ -26,94 +26,54 @@ public class HotelListService {
 	public long betweenTime(String firstDate, String secondDate) {
 	      try {
 	         String sd1 = firstDate.substring(0, 4);
-
-	         String sd2 = firstDate.substring(4, 6);
-
-	         String sd3 = firstDate.substring(6, 8);
-
+	         String sd2 = firstDate.substring(5, 7);
+	         String sd3 = firstDate.substring(8, 10);
 	         String ed1 = secondDate.substring(0, 4);
-
-	         String ed2 = secondDate.substring(4, 6);
-
-	         String ed3 = secondDate.substring(6, 8);
+	         String ed2 = secondDate.substring(5, 7);
+	         String ed3 = secondDate.substring(8, 10);
 
 	         firstDate = sd1 + "-" + sd2 + "-" + sd3;
-
 	         secondDate = ed1 + "-" + ed2 + "-" + ed3;
 
 	         SimpleDateFormat fm = new SimpleDateFormat("yyyy-MM-dd");
-
 	         Date sDate = fm.parse(firstDate);
-
 	         Date eDate = fm.parse(secondDate);
 
 	         long diff = eDate.getTime() - sDate.getTime();
-
 	         long diffDays = diff / (24 * 60 * 60 * 1000);
-
 	         long difMonth = (diffDays + 1) / 30; // 총개월수 ( 대략 30으로 나눴을때 나오는 개월수 )
-
 	         long chkNum = 0;
 
 	         int j = 0;
 
-	         // 개월수 체크 ( 시작한날짜월부터 위에서 대충 계산한 개월수까지 )
-
-	         // 각 월별로 해당하는 월수에 맞게 더해줌
-
 	         for (int i = Integer.parseInt(sd2); j < difMonth; i++) {
-
 	            if (i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 || i == 12) {
-
 	               chkNum += 31;
-
 	            } else if (i == 4 || i == 6 || i == 9 || i == 11) {
-
 	               chkNum += 30;
-
 	            }
 
 	            if (i == 2) {
-
-	               // 윤달체크
-
 	               if (((Integer.parseInt(sd2)) % 400) == 0) {
-
 	                  chkNum += 29;
-
 	               } else {
-
 	                  chkNum += 28;
-
 	               }
-
 	            }
-
 	            j++;
-
 	            if (i > 12) {
 	               i = 1;
 	               j = j - 1;
 	            }
-
 	         }
 
 	         long allMonth = (chkNum + 1) / 30;
-
 	         if (diffDays < chkNum) {
-
 	            allMonth = allMonth - 1;
-
 	         }
 
 	         System.out.println("날짜차이 =" + diffDays);
 
-	         System.out.println("총 차이  =" + chkNum);
-
-	         System.out.println("개월수 =" + difMonth);
-
-	         System.out.println("진짜개월수 =" + allMonth);
-	         
 	         return diffDays;
 
 	      } catch (Exception e) {
